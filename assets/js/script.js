e74113396e829c840205d18f2a6a14a66a878764
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-06-03 12:51:01 +0000

    added a function that generates an anagram

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,32 @@ const vowels = ['A', 'E', 'I', 'O', 'U'];
 const consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
 
 let anagram = [];
+let anagramArray = [];
 let vowelsUsed = [];
 let consonantsUsed = [];
 
 let vNum = 3;
 let cNum = 6;
 
+//  --------Event Listeners:
 
+$("#enterButton").on("click", anagramGenerator);
+$("#deleteButton").on("click", vowelGenerator);
 
 //  --------Functions:
 
-function vowelGenerator(vNum) {
+
+function vowelGenerator() {
+    console.log(anagramArray)
+}
+
+function anagramGenerator() {
+
+    //Clears all arrays so the anagram stays the desired number of characters
+    anagramArray = [];
+    vowelsUsed = [];
+    consonantsUsed = [];
+
     // Generates a number of random non-repeating vowels
     while (vowelsUsed.length < vNum) {
         let vowelIndexer = Math.floor(Math.random() * 5);
@@ -22,24 +37,43 @@ function vowelGenerator(vNum) {
             vowelsUsed.push(vowels[vowelIndexer])
         }
     }
-}
 
-function consonantGenerator(cNum) {
     // Generates a number of random non-repeating consonants
     while (consonantsUsed.length < cNum) {
         let consonantIndexer = Math.floor(Math.random() * 21);
         if (consonantsUsed.includes(consonants[consonantIndexer]) === false) {
             consonantsUsed.push(consonants[consonantIndexer])
+
         }
     }
+
+    //The consonants and vowels generated are concatenated into an array
+    anagramArray = vowelsUsed.concat(consonantsUsed);
+
+
+    // Then they are put through a Fisher-Yates algorithm to mix the vowels and adjectives
+    for (var i = anagramArray.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1)); //random index
+        [anagramArray[i], anagramArray[j]] = [anagramArray[j], anagramArray[i]]; // swap
+    }
+    console.log(anagramArray);
+    
+
 }
 
-function arrayShuffler(array) {
-    
+
+
+
+function consonantGenerator(cNum) {
+
 }
 
-function arrayToStringConverter(array) {
-    
+function arrayShuffler(anagramArray) {
+
+}
+
+function arrayToStringConverter(anagram) {
+
 }
 
 function gameGenerator(diffculty, time) {

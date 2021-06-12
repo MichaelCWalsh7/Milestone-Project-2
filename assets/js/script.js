3e74e60f4949d7c989c829908a55ba9359b5c0a4
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-06-12 20:33:02 +0000

    Change time limit

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,8 +145,8 @@ function deleteLetter() {
 
 
 function startTimer() {
-    var minute = 1;
-    var sec = 59;
+    var minute = 2;
+    var sec = 30;
     setInterval(function () {
         if (sec < 10) {
             document.getElementById("timer").innerHTML = minute + ":" + "0" + sec;
@@ -155,7 +155,7 @@ function startTimer() {
             document.getElementById("timer").innerHTML = minute + ":" + sec;
             sec--;
         } 
-        
+
         if (sec == 00 && minute != 0) {
             minute--;
             sec = 59;

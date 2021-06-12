52d973b9f548fa1f5e33eeb6d0e025b7c399a783
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-06-12 16:57:49 +0000

    Change consonants that can appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ function gameStart() {
 function anagramGenerator() {
 
     const vowels = ['A', 'E', 'I', 'O', 'U'];
-    const consonantsEasy = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'W'];
+    const consonantsEasy = ['B', 'C', 'D', 'F', 'G', 'H', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'Y'];
     const consonantsHard = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
 
     // Initialises block scope variables needed to succinctly generate an anagram. 
